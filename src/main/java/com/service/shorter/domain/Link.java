d4adb99442fcfc79e35e1690f02398cdd11e6d88
abdelrahman_Nasr <anasr@diginationmea.com>
2024-03-30 14:31:27 +0200

    change

--- src/main/java/com/service/shorter/domain/Link.java
+++ src/main/java/com/service/shorter/domain/Link.java
@@ -21,21 +21,15 @@ public class Link extends BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     Long id;
-
     @Column(name = "original_link")
     String originalLink;
-
     @Column(name = "short_link")
     String shortLink;
-
-    @Column(name = "generated_code")
+    @Column(name = "generated_code", unique = true)
     String generatedCode;
-
     @Column(name = "generated_link")
     String generatedLink;
-
     String uuid;
-
     int iterations;
     @Column(name = "hashed_code")
     int hashedCode;
